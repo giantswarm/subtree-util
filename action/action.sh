bd06868494f4e93e84e2d586fa810bbcb975e457
Gerald Pape <gerald@giantswarm.io>
2021-05-03 10:58:20 +0200

    First changes for making the action work for root

--- action/action.sh
+++ action/action.sh
@@ -83,8 +83,8 @@ pr_info=$(echo "$pr_list" | jq -cjr 'length, ",", .[1].additions | tostring')
 
 
 # if there are
-# no open prs from automation (0,null)
-# one open pr from automation and no external changes (1,1)
+# - no open prs from automation (0,null)
+# - one open pr from automation and no external changes (1,1)
 if [[ "${pr_info}" == "1,1" || "${pr_info}" == "0,null" ]]; then
 
   # clone the target repository
@@ -96,17 +96,24 @@ if [[ "${pr_info}" == "1,1" || "${pr_info}" == "0,null" ]]; then
   # determine default branch of our repo
   target_default_branch=$(git -C .target-repo remote show origin | grep "HEAD branch" | sed "s/.*: //")
 
-  # go to the default branch of the upstream-copy remote
-  git -C .target-repo checkout "upstream-copy/${default_branch}"
+  # if the source path is the root of the repository, we can skip
+  # the subtree split and git subtree merge directly into the target
+  if [[ "${SOURCE_PATH}" == "." ]]; then
+    # git subtree merge the upstream-copy default branch
+    git -C .target-repo subtree merge --squash -P "${TARGET_PATH}" "upstream-copy/${default_branch}"
+  else
+    # go to the default branch of the upstream-copy remote
+    git -C .target-repo checkout "upstream-copy/${default_branch}"
 
-  # extract the path we require into a branch named temp-split-branch
-  git -C .target-repo subtree split -P "${SOURCE_PATH}" -b temp-split-branch
+    # extract the path we require into a branch named temp-split-branch
+    git -C .target-repo subtree split -P "${SOURCE_PATH}" -b temp-split-branch
 
-  # create the pr branch from the default branch of the repository
-  git -C .target-repo checkout -b update-from-upstream "${target_default_branch}"
+    # create the pr branch from the default branch of the repository
+    git -C .target-repo checkout -b update-from-upstream "${target_default_branch}"
 
-  # merge back from the temp-split-branch
-  git -C .target-repo subtree merge --squash -P "${TARGET_PATH}" temp-split-branch
+    # merge back from the temp-split-branch
+    git -C .target-repo subtree merge --squash -P "${TARGET_PATH}" temp-split-branch
+  fi
 
   # push changes into branch
   git -C .target-repo subtree push origin update-from-upstream
@@ -114,6 +121,6 @@ if [[ "${pr_info}" == "1,1" || "${pr_info}" == "0,null" ]]; then
   if [[ "${pr_info}" == "0,null" ]]; then
     gh --repo "${TARGET_REPOSITORY}" pr create --title "Update from upstream" --base "${target_default_branch}" --head update-from-upstream --label "automated-update" --body "This PR has been created from automation in https://github.com/${GITHUB_REPOSITORY}"
   else
-    gh --repo "${TARGET_REPOSITORY}" pr "update-from-upstream" --body "Force pushed through automation"
+    gh --repo "${TARGET_REPOSITORY}" pr "update-from-upstream" comment --body "Force pushed through automation"
   fi
 fi
