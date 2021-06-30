fccbda9639ca99bee94dd728d31f30fe19ae9ac7
Łukasz Piątkowski <piontec@gmail.com>
2021-06-30 13:48:33 +0200

    add support for tags and branches; add support for add/merge in subtree

--- update-from-fork.sh
+++ update-from-fork.sh
@@ -13,15 +13,27 @@ fi
 # positional parameters
 fork_clone_url=${1:-}
 fork_chart_path=${2:-}
-target_app_path=${3:-}
-pr_branch=${4:-}
-if [[ -z "${fork_clone_url}" || -z "${fork_chart_path}" || -z "${target_app_path}" || -z "${pr_branch}" ]]; then
-  echo "usage: $0 fork_git-clone-url fork_chart_path target_app_path pr_branch_name"
+fork_target_ref=${3:-}
+target_app_path=${4:-}
+pr_branch=${5:-}
+subtree_mode=${6:-"merge"}
+if [[ -z "${fork_clone_url}" || -z "${fork_chart_path}" || -z "${fork_target_ref}" || -z "${target_app_path}" || -z "${pr_branch}" ]]; then
+  echo "usage: $0 fork_git-clone-url fork_chart_path fork_target_ref target_app_path pr_branch_name subtree_mode"
+  echo "fork_target_ref = 'tag:REMOTE_TAG|branch:REMOTE_BRANCH'"
+  echo "subtree_mode = [add|merge]"
   exit 1
 fi
 
-if [ ! -d "${target_app_path}" ]; then
+fork_target_type=${fork_target_ref%:*}
+fork_target_name=${fork_target_ref#*:}
+
+if [ "${subtree_mode}" == "merge" && ! -d "${target_app_path}" ]; then
   echo -e "Error:\n'${target_app_path}' target directory not found."
+  exit 1
+fi
+
+if [ "${subtree_mode}" == "add" && -d "${target_app_path}" ]; then
+  echo -e "Error:\n'${target_app_path}' target directory already exists."
   exit 1
 fi
 
@@ -44,7 +56,6 @@ fork_repo_name="${fork_repo_name%.git}"
 
 # query the forks default branch, we need it for setting up the remote and other stuff
 fork_repo_info=$(curl --silent -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${ORG}/${fork_repo_name}")
-fork_default_branch=$(echo "$fork_repo_info" | jq -cr ".default_branch")
 
 # determine default branch of our repo
 default_branch=$(git remote show origin | grep "HEAD branch" | sed "s/.*: //")
@@ -55,11 +66,19 @@ git checkout "${default_branch}"
 # update the default branch
 git pull origin "${default_branch}"
 
-# fetch the most recent state from the fork
-git fetch upstream-copy "${fork_default_branch}"
+# fetch the named branched or tag state from the fork
+# checkout the named branched or tag state from the fork
+if [[ "${fork_target_type}" == "tag" ]]; then
+  # we might already have the tag and git will complain
+  set +e
+  git fetch upstream-copy --tags "${fork_target_name}"
+  set -e
+  git checkout "${fork_target_name}"
+else
+  git fetch upstream-copy "${fork_target_name}"
+  git checkout "upstream-copy/${fork_target_name}"
+fi
 
-# go to the default branch of the upstream-copy remote
-git checkout "upstream-copy/${fork_default_branch}"
 
 # extract the path we require into a branch named temp-split-branch
 git subtree split -P "${fork_chart_path}" -b temp-split-branch
@@ -68,6 +87,6 @@ git subtree split -P "${fork_chart_path}" -b temp-split-branch
 git checkout -b "${pr_branch}" "${default_branch}"
 
 # merge back from the temp-split-branch
-git subtree -d merge --squash -P "${target_app_path}" temp-split-branch
+git subtree -d "${subtree_mode}" --squash -P "${target_app_path}" temp-split-branch
 
 git branch -D temp-split-branch
